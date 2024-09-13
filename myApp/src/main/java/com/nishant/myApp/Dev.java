55d5d4baf91f62539cb718ad65f08d9be150ff38
Nishant Raj <nishantraj85297@gmail.com>
2024-09-13 17:45:58 +0530

    Dependency Injection using Spring Boot

---
+++ myApp/src/main/java/com/nishant/myApp/Dev.java
@@ -0,0 +1,12 @@
+package com.nishant.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build()
+    {
+        System.out.println("working on awesome project");
+    }
+}
